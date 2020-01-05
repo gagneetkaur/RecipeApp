5a4f0a23ff12c8e87bd74feb1154a47f88b36fb1
gagneet kaur <gagneet.kaur@comprotechnologies.com>
2020-01-06 01:15:15 +0530

    Initial commit Angular

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,13 @@
+import { ThrowStmt } from '@angular/compiler';
+
+export class Recipe{
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, description: string, imagePath: string){
+this.name = name;
+this.description =description;
+this.imagePath = imagePath;
+    }
+}
